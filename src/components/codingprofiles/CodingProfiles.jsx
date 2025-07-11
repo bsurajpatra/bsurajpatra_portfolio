69c185b7a2104316b4f8936c8a6c6b4563cb7f46
bsurajpatra <ankitsuraj1111@gmail.com>
2025-07-11 16:44:07 +0530

    Changed username for gfg

--- src/components/codingprofiles/CodingProfiles.jsx
+++ src/components/codingprofiles/CodingProfiles.jsx
@@ -24,7 +24,7 @@ const CodingProfiles = () => {
         {
             id: 3,
             platform: "GeeksforGeeks",
-            username: "user_n7dd51vjk9y",
+            username: "bsurajpatra",
             logo: geeksforgeeksLogo,
             url: "https://www.geeksforgeeks.org/user/user_n7dd51vjk9y/"
         }
