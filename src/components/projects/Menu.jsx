9c71d53e6b513ffa9fef543fe218138a88c80d6f
bsurajpatra <ankitsuraj1111@gmail.com>
2025-07-17 16:21:49 +0530

    Updated menu

--- src/components/projects/Menu.jsx
+++ src/components/projects/Menu.jsx
@@ -1,9 +1,28 @@
 import KLEats from "../../assets/kleats.png";
 import Portfolio from "../../assets/portfolio.png";
 import KarmaSync from "../../assets/KarmaSync.png";
-import SafeWebVerify from "../../assets/safewebverify.png";
+import SafeWebVerify from "../../assets/safewebverify.jpg";
+
 
 const Menu = [
+	{
+		id: 4,
+		image: SafeWebVerify,
+		title: "SafeWebVerify",
+		category: ["MERN, AI/ML"],
+		url: null,
+		repositoryUrl: "https://github.com/bsurajpatra/SafeWebVerify",
+		description: "SafeWebVerify is an AI/ML-powered web app that detects phishing websites in real-time with 98% accuracy."
+	},
+	{
+		id: 3,
+		image: KarmaSync,
+		title: "KarmaSync",
+		category: ["MERN"],
+		url: "https://karmasync.vercel.app/",
+		repositoryUrl: "https://github.com/bsurajpatra/KarmaSync_info",
+		description: "An intuitive project management tool to organize, assign, and complete tasks with ease."
+	},
 	{
 		id: 1,
 		image: KLEats,
@@ -17,31 +36,12 @@ const Menu = [
 		id: 2,
 		image: Portfolio,
 		title: "Personal Portfolio",
-		category: ["MERN"],
+		category: ["React"],
 		url: "https://bsurajpatra-portfolio.vercel.app/",
 		repositoryUrl: "https://github.com/bsurajpatra/bsurajpatra_portfolio",
 		description: "A personal portfolio showcasing my skills, projects, and experience."
-	},
-	{
-		id: 3,
-		image: KarmaSync,
-		title: "KarmaSync",
-		category: ["MERN"],
-		url: "https://karmasync.vercel.app/",
-		repositoryUrl: "https://github.com/bsurajpatra/KarmaSync_info",
-		description: "An intuitive project management tool to organize, assign, and complete tasks with ease."
-	},
-	{
-		id: 4,
-		image: SafeWebVerify,
-		title: SafeWebVerify,
-		category: ["MERN, AI/ML"],
-		url: "",
-		repositoryUrl: "https://github.com/bsurajpatra/SafeWebVerify",
-		description: "SafeWebVerify is an AI/ML-powered web app that detects phishing websites in real-time with 98% accuracy."
 	}
-
-
 ];
+
 
 export default Menu;
