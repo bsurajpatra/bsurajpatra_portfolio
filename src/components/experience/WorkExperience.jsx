e653cea3348721a42735e664709a58e71721239a
bsurajpatra <ankitsuraj1111@gmail.com>
2025-09-17 17:03:59 +0530

    Added kleats experience

--- src/components/experience/WorkExperience.jsx
+++ src/components/experience/WorkExperience.jsx
@@ -1,9 +1,23 @@
+import KLEats from '../../assets/kleatslogo.png';
 import Swecha from '../../assets/swecha.png';
 import SwechaCertificate from '../../assets/swecha-certificate.jpg';
 
 const WorkExperience = [
     {
         id: 1,
+        title: "Scrum Master & Developer",
+        company: "KL Eats",
+        yearsActive: "July 2024 - Present",
+        location: "Remote",
+        image: { src: KLEats, style: { width: '100px', height: 'auto' } },
+        information: [
+            "Product of KLGLUG, Unit of Quitech Labs Pvt Ltd.",
+            "Led agile development as Scrum Master, coordinating sprint planning, reviews, and team collaboration.",
+            "Designed and developed features for a food ordering and payment platform using modern web technologies."
+        ],
+    },
+    {
+        id: 2,
         title: "Open Data Intern",
         company: "SwechaAP",
         yearsActive: "May 2024 - June 2024",
