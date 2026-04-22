2112c8afad2f5b7e91040c83b6606b8757586049
bsurajpatra <ankitsuraj1111@gmail.com>
2026-04-22 22:05:41 +0530

    Updated hero section logo

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
-import Me from '../../assets/hero.webp';
+import Me from '../../assets/hero.png';
 import Image from 'next/image';
 import ScrollDown from './ScrollDown';
 import dynamic from 'next/dynamic';
